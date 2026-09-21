ef2261f190d23ba198a21903664b32c33ee32a9a
Oscar R. <o.ruiz6048@student.sbccd.edu>
2026-09-21 09:13:42 -0700

    First try with implementing gooey GUI

--- source/main.py
+++ source/main.py
@@ -1,8 +1,14 @@
 import menu
 from Memory import populate
+from gooey import Gooey
+from argparse import ArgumentParser
+@Gooey
 def main():
     populate()
     menu.menu()
+    parser = ArgumentParser(description="My Cool GUI Program!")
+    parser.add_argument('Filename', widget="FileChooser")
+    parser.parse_args()
 
 
 
